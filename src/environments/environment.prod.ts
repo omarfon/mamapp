56b5fd57bd688504de44904f56031cc98f66c58d
omar <magentadin@gmail.com>
2020-05-05 16:47:10 -0500

    modificando environmets dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -23,9 +23,9 @@ export const environment = {
 };
 
 /* ENDPOINTS DESARROLLO */
-export const API_ENDPOINT = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/api/v2/"; 
-export const API_NOTAS = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/api/v2/mama/xnotas";  
-export const API_IMAGES = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/img/professionals/"; 
+export const API_ENDPOINT = "https://dappapache02.eastus.cloudapp.azure.com/middleware2/api/v2/"; 
+export const API_NOTAS = "https://dappapache02.eastus.cloudapp.azure.com/middleware2/api/v2/mama/";  
+export const API_IMAGES = "https://dappapache02.eastus.cloudapp.azure.com/middleware2/img/professionals/"; 
 
 
 /* ENDPOINTS PRODUCCION */
